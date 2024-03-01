175291a462f32fa13cd3126e6e4d7ada67c2f82d
Fernando Castro <devfercastro@gmail.com>
2024-03-01 17:21:44 -0300

    Postgres database conection

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Cafesitopara@fedora39',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
