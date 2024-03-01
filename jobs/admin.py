f5d4155e6896a5bbbd962eb6862c5aa70f4459ef
Fernando Castro <devfercastro@gmail.com>
2024-03-01 18:04:11 -0300

    Customizing the jobs view into admin panel

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Job
 
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("id", "image", "summary", )
+
+admin.site.register(Job, JobAdmin)
